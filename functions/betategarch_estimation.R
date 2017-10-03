fc29d18ea0ba77618526e2fae15c89a98b133d05
daianemarcolino <daimmattos@gmail.com>
2017-10-03 19:04:21 -0300

    betategarch_estimation com dummies

--- functions/betategarch_estimation.R
+++ functions/betategarch_estimation.R
@@ -1,4 +1,4 @@
-betategarch_estimation <- function(y, initial = NULL, type = "var"){
+betategarch_estimation <- function(y, initial = NULL, type = "var", dummy = NULL){
   
   
   if(type == "var"){ # modelo y[t] = exp(f2[t])*epsilon[t], epsilon[t] ~ t(v)
@@ -96,7 +96,7 @@ betategarch_estimation <- function(y, initial = NULL, type = "var"){
     # output
     print(otimizados)
     invisible(list(out = out, otimizados = otimizados, loglik = -loglik))
-
+    
   }else if(type == "mean-var2"){ # modelo y[t] = f1[t] + exp(f2[t])*epsilon[t], epsilon[t] ~ t(v)
     # > estimação via ML para densidade condicional de y t-student com média constante e variância variante no tempo
     otimizar <- function(y, par){
@@ -167,71 +167,295 @@ betategarch_estimation <- function(y, initial = NULL, type = "var"){
       w2 <- par[2]
       A1_00 <- par[3]
       A1_01 <- par[4]
-      A1_11 <- 0
-      A1_12 <- 0
       B1_00 <- par[5]
-      B1_01 <- par[6]
-      B1_11 <- 0
-      B1_12 <- 0
-      A2_00 <- par[7]
-      B2_00 <- par[8]
-      df <- par[9]
-      f1 <- rep(w1/(1 - B1_00),13)
-      f2 <- rep(w2/(1 - B2_00),13)
+      A2_00 <- par[6]
+      B2_00 <- par[7]
+      df <- par[8]
+      f1 <- rep(w1/(1 - B1_00),2)
+      f2 <- rep(w2/(1 - B2_00),2)
       N <- length(y)
       u1 <- NULL
       u2 <- NULL
-      for(t in 1:12){
+      t = 1
       u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
       u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
-      }
-      for(t in 13:N){
+      for(t in 2:N){
         u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
         u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
-        f1[t+1] <- w1 + A1_00*u1[t] + A1_01*u1[t-1] + A1_11*u1[t-11] + A1_12*u1[t-12] + B1_00*f1[t] + B1_01*f1[t-1] + B1_11*f1[t-11] + B1_12*f1[t-12]
+        f1[t+1] <- w1 + A1_00*u1[t] + A1_01*u1[t-1] + B1_00*f1[t]
         f2[t+1] <- w2 + A2_00*u2[t] + B2_00*f2[t]
       }
       
-      f1 <- ts(f1[13:N], start = start(y), freq = frequency(y))
-      f2 <- ts(f2[13:N], start = start(y), freq = frequency(y))
+      f1 <- ts(f1[2:N], end = end(y), freq = frequency(y))
+      f2 <- ts(f2[2:N], end = end(y), freq = frequency(y))
       
       # loglik
       loglik <- N*log(gamma((df+1)/2)) - (N/2)*log(pi) - N*log(gamma(df/2)) - (N/2)*log(df) - sum(f2) - ((df + 1)/2)*sum(log(1 + (y - f1)^2/(df*exp(2*f2))))
       -loglik
     }
     
+    otimizados <- nlminb(start = c(initial), objective = otimizar, y = y,  
+                         lower = c(-Inf,-Inf,-Inf,-Inf,-1,-Inf,-1,2), upper = c(Inf,Inf,Inf,Inf,1,Inf,1,Inf), control = list(eval.max = 10000, iter.max = 10000))
     
-    otimizados <- nlminb(start = c(initial), objective = otimizar, y = y, 
+    w1 <- otimizados$par[1]
+    w2 <- otimizados$par[2]
+    A1_00 <- otimizados$par[3]
+    A1_01 <- otimizados$par[4]
+    B1_00 <- otimizados$par[5]
+    A2_00 <- otimizados$par[6]
+    B2_00 <- otimizados$par[7]
+    df <- otimizados$par[8]
+    f1 <- rep(w1/(1 - B1_00),2)
+    f2 <- rep(w2/(1 - B2_00),2)
+    N <- length(y)
+    u1 <- NULL
+    u2 <- NULL
+    t = 1
+    u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+    u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+    for(t in 2:N){
+      u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+      u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+      f1[t+1] <- w1 + A1_00*u1[t] + A1_01*u1[t-1] + B1_00*f1[t]
+      f2[t+1] <- w2 + A2_00*u2[t] + B2_00*f2[t]
+    }
+    f1 <- ts(f1[2:N], end = end(y), freq = frequency(y))
+    f2 <- ts(f2[2:N], end = end(y), freq = frequency(y))
+    
+    loglik <- N*log(gamma((df+1)/2)) - (N/2)*log(pi) - N*log(gamma(df/2)) - (N/2)*log(df) - sum(f2) - ((df + 1)/2)*sum(log(1 + (y - f1)^2/(df*exp(2*f2))))
+    epsilon <- (y - f1)/exp(f2)
+    out <- cbind(f1, f2, exp(f2), epsilon)
+    colnames(out) <- c("f1", "f2", "sigma","epsilon")
+    
+    # output
+    print(otimizados)
+    invisible(list(out = out, otimizados = otimizados, loglik = -loglik))
+    
+  }else if(type == "mean-var4"){ # modelo y[t] = f1[t] + exp(f2[t])*epsilon[t], epsilon[t] ~ t(v)
+    # > estimação via ML para densidade condicional de y t-student com média constante e variância variante no tempo
+    otimizar <- function(y, par){
+      w1 <- par[1]
+      w2 <- par[2]
+      A1_00 <- par[3]
+      A1_01 <- par[4]
+      B1_00 <- par[5]
+      B1_01 <- par[6]
+      A2_00 <- par[7]
+      B2_00 <- par[8]
+      df <- par[9]
+      f1 <- rep(w1/(1 - B1_00 - B1_01),2)
+      f2 <- rep(w2/(1 - B2_00),2)
+      N <- length(y)
+      u1 <- NULL
+      u2 <- NULL
+      t = 1
+      u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+      u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+      for(t in 2:N){
+        u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+        u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+        f1[t+1] <- w1 + A1_00*u1[t] + A1_01*u1[t-1] + B1_00*f1[t] + B1_01*f1[t-1]
+        f2[t+1] <- w2 + A2_00*u2[t] + B2_00*f2[t]
+      }
+      
+      f1 <- ts(f1[2:N], end = end(y), freq = frequency(y))
+      f2 <- ts(f2[2:N], end = end(y), freq = frequency(y))
+      
+      # loglik
+      loglik <- N*log(gamma((df+1)/2)) - (N/2)*log(pi) - N*log(gamma(df/2)) - (N/2)*log(df) - sum(f2) - ((df + 1)/2)*sum(log(1 + (y - f1)^2/(df*exp(2*f2))))
+      -loglik
+    }
+    
+    otimizados <- nlminb(start = c(initial), objective = otimizar, y = y,  
                          lower = c(-Inf,-Inf,-Inf,-Inf,-1,-1,-Inf,-1,2), upper = c(Inf,Inf,Inf,Inf,1,1,Inf,1,Inf), control = list(eval.max = 10000, iter.max = 10000))
     
     w1 <- otimizados$par[1]
     w2 <- otimizados$par[2]
     A1_00 <- otimizados$par[3]
     A1_01 <- otimizados$par[4]
-    A1_11 <- 0
-    A1_12 <- 0
     B1_00 <- otimizados$par[5]
     B1_01 <- otimizados$par[6]
-    B1_11 <- 0
-    B1_12 <- 0
     A2_00 <- otimizados$par[7]
     B2_00 <- otimizados$par[8]
     df <- otimizados$par[9]
-    f1 <- rep(w1/(1 - B1_00),13)
-    f2 <- rep(w2/(1 - B2_00),13)
+    f1 <- rep(w1/(1 - B1_00 - B1_01),2)
+    f2 <- rep(w2/(1 - B2_00),2)
     N <- length(y)
     u1 <- NULL
     u2 <- NULL
-    for(t in 1:12){
+    t = 1
+    u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+    u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+    for(t in 2:N){
       u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
       u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
-    }
-    for(t in 13:N){
-      u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
-      u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
-      f1[t+1] <- w1 + A1_00*u1[t] + A1_01*u1[t-1] + A1_11*u1[t-11] + A1_12*u1[t-12] + B1_00*f1[t] + B1_01*f1[t-1] + B1_11*f1[t-11] + B1_12*f1[t-12]
+      f1[t+1] <- w1 + A1_00*u1[t] + A1_01*u1[t-1] + B1_00*f1[t] + B1_01*f1[t-1]
       f2[t+1] <- w2 + A2_00*u2[t] + B2_00*f2[t]
     }
+    f1 <- ts(f1[2:N], end = end(y), freq = frequency(y))
+    f2 <- ts(f2[2:N], end = end(y), freq = frequency(y))
+    
+    loglik <- N*log(gamma((df+1)/2)) - (N/2)*log(pi) - N*log(gamma(df/2)) - (N/2)*log(df) - sum(f2) - ((df + 1)/2)*sum(log(1 + (y - f1)^2/(df*exp(2*f2))))
+    epsilon <- (y - f1)/exp(f2)
+    out <- cbind(f1, f2, exp(f2), epsilon)
+    colnames(out) <- c("f1", "f2", "sigma","epsilon")
+    
+    # output
+    print(otimizados)
+    invisible(list(out = out, otimizados = otimizados, loglik = -loglik))
+    
+  }else if(type == "mean-var5"){ # modelo y[t] = f1[t] + exp(f2[t])*epsilon[t], epsilon[t] ~ t(v)
+    # > estimação via ML para densidade condicional de y t-student com média constante e variância variante no tempo
+    otimizar <- function(y, par){
+      w1 <- par[1]
+      w2 <- par[2]
+      A1_00 <- par[3]
+      A1_01 <- par[4]
+      A1_11 <- par[5]
+      B1_00 <- par[6]
+      B1_01 <- par[7]
+      A2_00 <- par[8]
+      B2_00 <- par[9]
+      df <- par[10]
+      f1 <- rep(w1/(1 - B1_00 - B1_01),12)
+      f2 <- rep(w2/(1 - B2_00),12)
+      N <- length(y)
+      u1 <- NULL
+      u2 <- NULL
+      for(t in 1:11){
+        u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+        u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+      }
+      for(t in 12:N){
+        u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+        u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+        f1[t+1] <- w1 + A1_00*u1[t] + A1_01*u1[t-1] + A1_11*u1[t-11] + B1_00*f1[t] + B1_01*f1[t-1]
+        f2[t+1] <- w2 + A2_00*u2[t] + B2_00*f2[t]
+      }
+      
+      f1 <- ts(f1[13:N], end = end(y), freq = frequency(y))
+      f2 <- ts(f2[13:N], end = end(y), freq = frequency(y))
+      
+      # loglik
+      loglik <- N*log(gamma((df+1)/2)) - (N/2)*log(pi) - N*log(gamma(df/2)) - (N/2)*log(df) - sum(f2) - ((df + 1)/2)*sum(log(1 + (y - f1)^2/(df*exp(2*f2))))
+      -loglik
+    }
+    
+    otimizados <- nlminb(start = c(initial), objective = otimizar, y = y,  
+                         lower = c(-Inf,-Inf,-Inf,-Inf,-Inf,-1,-1,-Inf,-1,2), upper = c(Inf,Inf,Inf,Inf,Inf,1,1,Inf,1,Inf), control = list(eval.max = 10000, iter.max = 10000))
+    
+    w1 <- otimizados$par[1]
+    w2 <- otimizados$par[2]
+    A1_00 <- otimizados$par[3]
+    A1_01 <- otimizados$par[4]
+    A1_11 <- otimizados$par[5]
+    B1_00 <- otimizados$par[6]
+    B1_01 <- otimizados$par[7]
+    A2_00 <- otimizados$par[8]
+    B2_00 <- otimizados$par[9]
+    df <- otimizados$par[10]
+    f1 <- rep(w1/(1 - B1_00 - B1_01),12)
+    f2 <- rep(w2/(1 - B2_00),12)
+    N <- length(y)
+    u1 <- NULL
+    u2 <- NULL
+    for(t in 1:11){
+      u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+      u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+    }
+    for(t in 12:N){
+      u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+      u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+      f1[t+1] <- w1 + A1_00*u1[t] + A1_01*u1[t-1] + A1_11*u1[t-11] + B1_00*f1[t] + B1_01*f1[t-1]
+      f2[t+1] <- w2 + A2_00*u2[t] + B2_00*f2[t]
+    }
+    
+    f1 <- ts(f1[13:N], end = end(y), freq = frequency(y))
+    f2 <- ts(f2[13:N], end = end(y), freq = frequency(y))
+    
+    loglik <- N*log(gamma((df+1)/2)) - (N/2)*log(pi) - N*log(gamma(df/2)) - (N/2)*log(df) - sum(f2) - ((df + 1)/2)*sum(log(1 + (y - f1)^2/(df*exp(2*f2))))
+    epsilon <- (y - f1)/exp(f2)
+    out <- cbind(f1, f2, exp(f2), epsilon)
+    colnames(out) <- c("f1", "f2", "sigma","epsilon")
+    
+    # output
+    print(otimizados)
+    invisible(list(out = out, otimizados = otimizados, loglik = -loglik))
+    
+  }else if(type == "mean-var6"){ # modelo y[t] = f1[t] + exp(f2[t])*epsilon[t], epsilon[t] ~ t(v) c/ dummy
+    # > estimação via ML para densidade condicional de y t-student com média constante e variância variante no tempo
+    otimizar <- function(y, par){
+      w1 <- par[1]
+      w2 <- par[2]
+      A1_00 <- par[3]
+      A1_01 <- par[4]
+      A1_11 <- par[5]
+      B1_00 <- par[6]
+      B1_01 <- par[7]
+      A2_00 <- par[8]
+      B2_00 <- par[9]
+      df <- par[10]
+      D1 <- par[11]
+      D2 <- par[12]
+      D3 <- par[13]
+      D4 <- par[14]
+      f1 <- rep(w1/(1 - B1_00 - B1_01),12)
+      f2 <- rep(w2/(1 - B2_00),12)
+      N <- length(y)
+      u1 <- NULL
+      u2 <- NULL
+      for(t in 1:11){
+        u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+        u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+      }
+      for(t in 12:(N-1)){
+        u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+        u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+        f1[t+1] <- w1 + A1_00*u1[t] + A1_01*u1[t-1] + A1_11*u1[t-11] + B1_00*f1[t] + B1_01*f1[t-1] + D1*dummy[t+1,1] + D2*dummy[t+1,2] + D3*dummy[t+1,3] + D4*dummy[t+1,4]
+        f2[t+1] <- w2 + A2_00*u2[t] + B2_00*f2[t]
+      }
+      
+      f1 <- ts(f1[13:N], end = end(y), freq = frequency(y))
+      f2 <- ts(f2[13:N], end = end(y), freq = frequency(y))
+      
+      # loglik
+      loglik <- N*log(gamma((df+1)/2)) - (N/2)*log(pi) - N*log(gamma(df/2)) - (N/2)*log(df) - sum(f2) - ((df + 1)/2)*sum(log(1 + (y - f1)^2/(df*exp(2*f2))))
+      -loglik
+    }
+    
+    otimizados <- nlminb(start = c(initial), objective = otimizar, y = y,  
+                         lower = c(-Inf,-Inf,-Inf,-Inf,-Inf,-1,-1,-Inf,-1,2,-Inf,-Inf,-Inf,-Inf), upper = c(Inf,Inf,Inf,Inf,Inf,1,1,Inf,1,Inf,Inf,Inf,Inf,Inf), control = list(eval.max = 10000, iter.max = 10000))
+    
+    w1 <- otimizados$par[1]
+    w2 <- otimizados$par[2]
+    A1_00 <- otimizados$par[3]
+    A1_01 <- otimizados$par[4]
+    A1_11 <- otimizados$par[5]
+    B1_00 <- otimizados$par[6]
+    B1_01 <- otimizados$par[7]
+    A2_00 <- otimizados$par[8]
+    B2_00 <- otimizados$par[9]
+    df <- otimizados$par[10]
+    D1 <- otimizados$par[11]
+    D2 <- otimizados$par[12]
+    D3 <- otimizados$par[13]
+    D4 <- otimizados$par[14]
+    f1 <- rep(w1/(1 - B1_00 - B1_01),12)
+    f2 <- rep(w2/(1 - B2_00),12)
+    N <- length(y)
+    u1 <- NULL
+    u2 <- NULL
+    for(t in 1:11){
+      u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+      u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+    }
+    for(t in 12:(N-1)){
+      u1[t] <- (df + 1)*((y[t] - f1[t])/(df*exp(2*f2[t])))/(1 + (y[t] - f1[t])^2/(df*exp(2*f2[t])))
+      u2[t] <- (((df + 1)*(y[t] - f1[t])^2) / (df*exp(2*f2[t]) + (y[t] - f1[t])^2)) - 1
+      f1[t+1] <- w1 + A1_00*u1[t] + A1_01*u1[t-1] + A1_11*u1[t-11] + B1_00*f1[t] + B1_01*f1[t-1] + D1*dummy[t+1,1] + D2*dummy[t+1,2] + D3*dummy[t+1,3] + D4*dummy[t+1,4]
+      f2[t+1] <- w2 + A2_00*u2[t] + B2_00*f2[t]
+    }
+    
     f1 <- ts(f1[13:N], end = end(y), freq = frequency(y))
     f2 <- ts(f2[13:N], end = end(y), freq = frequency(y))
     
@@ -244,4 +468,6 @@ betategarch_estimation <- function(y, initial = NULL, type = "var"){
     print(otimizados)
     invisible(list(out = out, otimizados = otimizados, loglik = -loglik))
   }
+  
+  
 }
--- scripts/03_betategarch_ipc.R
+++ scripts/03_betategarch_ipc.R
@@ -1,6 +1,7 @@
 # pacotes, funções e leitura
 library(betategarch)
 library(dygraphs)
+library(BETS)
 source("functions/betategarch_estimation.R")
 ipc <- ts(read.csv2("dados/VARIAÇÃO IPC-DI.csv")[,2], start = c(1999,1), freq = 12)
 
@@ -57,6 +58,10 @@ Box.test(m1$out[,"epsilon"], lag = 12, type = "Ljung-Box")
 Box.test(m2$out[,"epsilon"], lag = 12, type = "Ljung-Box")
 Box.test(m3$out[,"epsilon"], lag = 12, type = "Ljung-Box")
 
+# ACF
+acf(m3$out[,"epsilon"], lag.max = 48)
+m3$otimizados
+
 # salvar resultados para shiny
 resultados <- list(dygraphs_y = list(d1y = d1y, d2y = d2y, d3y = d3y),
                    dygraphs_ep = list(d1ep = d1ep, d2ep = d2ep, d3ep = d3ep),
@@ -65,8 +70,10 @@ saveRDS(resultados, "shiny_simulacao/data/resultados_betategarch_estimation.rds"
 
 
 
-# estimar modelo: variância + média, s(t-11) s(t-12)
-m4 <- betategarch_estimation(ipc, initial = c(0.05,0.05,0.05,0.5,0.5,0.5,0.5,0.5,4.3), type = "mean-var3")
+# estimar modelo: variância + média, s(t-1)
+d <- data.frame(param = c("w1", "w2",  "A1_00","A1_01","B1_00","B1_01","A2_00","B2_00","df"),
+                valor = c(0.21, -0.03,  0.05  ,0.05  ,0.57   ,0.02   ,0.97   , 4.73))
+m4 <- betategarch_estimation(ipc, initial = d[,2], type = "mean-var3")
 plot(m4$out)
 ver <- cbind(ipc,m4$out[,c("f1","sigma")])
 colnames(ver) <- c("ipc","f1","sigma")
@@ -80,4 +87,80 @@ d4y
 plot(m4$out[,"epsilon"])
 acf(m4$out[,"epsilon"], lag.max = 48)
 Box.test(m4$out[,"epsilon"], lag = 12, type = "Ljung-Box")
+m4$otimizados
 
+# estimar modelo: variância + média, s(t-1) f(t-1)
+d <- data.frame(param = c("w1", "w2",  "A1_00","A1_01","B1_00","B1_01","A2_00","B2_00","df"),
+                valor = c(0.29, -0.002,  0.04  ,0.01  ,0.40   ,0.5,   0.03   ,0.99   , 4.47))
+m5 <- betategarch_estimation(ipc, initial = d[,2], type = "mean-var4")
+plot(m5$out)
+ver <- cbind(ipc,m5$out[,c("f1","sigma")])
+colnames(ver) <- c("ipc","f1","sigma")
+d5y <- dygraph(ver) %>%
+  dySeries("ipc", label = "FGV IPC", color = "darkgrey", strokeWidth = 1) %>%
+  dySeries("f1", label = "Média", color = "steelblue", strokeWidth = 2) %>%
+  dySeries("sigma", label = "Sigma", color = "orangered", strokeWidth = 2) %>%
+  dyRangeSelector()
+d5y 
+
+plot(m5$out[,"epsilon"])
+acf(m5$out[,"epsilon"], lag.max = 48)
+Box.test(m5$out[,"epsilon"], lag = 12, type = "Ljung-Box")
+m5$otimizados
+
+# estimar modelo: variância + média, s(t-1) s(t-11) f(t-1)
+d <- data.frame(param = c("w1", "w2",  "A1_00","A1_01","A1_11","B1_00","B1_01","A2_00","B2_00","df"),
+                valor = c(0.29, -0.002,  0.04  ,0.01  ,0.05   , 0.40  ,0.5    ,0.03   ,0.99   , 4.47))
+m6 <- betategarch_estimation(ipc, initial = d[,2], type = "mean-var5")
+plot(m6$out)
+ver <- cbind(ipc,m6$out[,c("f1","sigma")])
+colnames(ver) <- c("ipc","f1","sigma")
+d6y <- dygraph(ver) %>%
+  dySeries("ipc", label = "FGV IPC", color = "darkgrey", strokeWidth = 1) %>%
+  dySeries("f1", label = "Média", color = "steelblue", strokeWidth = 2) %>%
+  dySeries("sigma", label = "Sigma", color = "orangered", strokeWidth = 2) %>%
+  dyRangeSelector()
+d6y 
+
+plot(m6$out[,"epsilon"])
+acf(m6$out[,"epsilon"], lag.max = 48)
+Box.test(m6$out[,"epsilon"], lag = 12, type = "Ljung-Box")
+m6$otimizados
+
+
+resp <- (m6$out[,"epsilon"] - mean(m6$out[,"epsilon"]))/sd(m6$out[,"epsilon"])
+dygraph(resp) %>%
+  dySeries("V1", label = "resíduo") %>%
+  dyShading(from = -3, to = 3, axis = "y")
+
+# OUTLIERS: JULHO DE 2000 E NOVEMBRO DE 2002
+d1 <- BETS.dummy(start = start(ipc), end = end(ipc), year = 2000, month = 7)
+d2 <- BETS.dummy(start = start(ipc), end = end(ipc), year = 2002, month = 11)
+d3 <- BETS.dummy(start = start(ipc), end = end(ipc), year = 2015, month = 1)
+d4 <- BETS.dummy(start = start(ipc), end = end(ipc), year = 2016, month = 1)
+dummies <- cbind(d1,d2,d3,d4)
+
+# estimar modelo: variância + média, s(t-1) s(t-11) f(t-1)
+d <- data.frame(param = c("w1", "w2",  "A1_00","A1_01","A1_11","B1_00","B1_01","A2_00","B2_00","df","D1","D2","D3","D4"),
+                valor = c(0.29, -0.002,  0.04  ,0.01  ,0.05   , 0.40  ,0.5    ,0.03   ,0.99   , 4.47, 1,1,1,1 ))
+m7 <- betategarch_estimation(ipc, initial = d[,2], type = "mean-var6", dummy = dummies)
+plot(m7$out)
+ver <- cbind(ipc,m7$out[,c("f1","sigma")])
+colnames(ver) <- c("ipc","f1","sigma")
+d7y <- dygraph(ver) %>%
+  dySeries("ipc", label = "FGV IPC", color = "darkgrey", strokeWidth = 1) %>%
+  dySeries("f1", label = "Média", color = "steelblue", strokeWidth = 2) %>%
+  dySeries("sigma", label = "Sigma", color = "orangered", strokeWidth = 2) %>%
+  dyRangeSelector()
+d7y 
+
+plot(m7$out[,"epsilon"])
+acf(m7$out[,"epsilon"], lag.max = 48)
+Box.test(m7$out[,"epsilon"], lag = 24, type = "Ljung-Box")
+m7$otimizados
+
+
+resp <- (m7$out[,"epsilon"] - mean(m7$out[,"epsilon"]))/sd(m7$out[,"epsilon"])
+dygraph(resp) %>%
+  dySeries("V1", label = "resíduo") %>%
+  dyShading(from = -3, to = 3, axis = "y")
