3345e85ca909df970e6e5fc7e9c0593223455737
daianemarcolino <daimmattos@gmail.com>
2017-11-23 16:13:36 -0200

    função para o diag dos núcleos (incompleta) vai até atração

--- functions/core.diag.R
+++ functions/core.diag.R
@@ -4,7 +4,7 @@ core.diag <- function(y, core, par, conf = 0.95){
   # lags: ur.df lag (numeric -> c(y, core, diff_y, diff_core))
   # type: ur.df type (trend, drift, none -> c(y, core, diff_y, diff_core))
   
-  data <- na.omit(cbind(ipc,fk2[,"mu"]))
+  data <- na.omit(cbind(y,core))
   y <- data[,1]
   core <- data[,2]
   
@@ -18,6 +18,7 @@ core.diag <- function(y, core, par, conf = 0.95){
   tabela_desc[,4] <- tabela_desc[,3]/tabela_desc[,1]
   tabela_desc[,5] <- c(NA, tabela_desc[-1,1]-tabela_desc[1,1])
   tabela_desc <- round(tabela_desc,4)
+  
   # diag 2: teste do viés --------
   
   # modelo irrestrito
@@ -53,7 +54,7 @@ core.diag <- function(y, core, par, conf = 0.95){
   }else{
     msg3 <- paste0("   Conclusion: don't reject H0 (",conf*100,"% confidence level)")
   }
-  pvalor <- pf(F0, 2, length(y) - 2)
+  pvalor <- 1 - pf(F0, 2, length(y) - 2)
 
   message("- ALPHA TEST -> H0: alpha = 0 \n", 
       paste("   coef:", round(coef[1],4)),
@@ -68,7 +69,7 @@ core.diag <- function(y, core, par, conf = 0.95){
   diag2 <- data.frame(stats = round(c(t_alpha, t_beta, F0),4),
              t_value = round(c(rep(t_test,2), NA),4),
              f_value = round(c(NA,NA,Fc),4))
-  diag2$conclusion <- ifelse(diag2$stats > diag2$t_value, "reject H0", "don't reject H0")
+  diag2$conclusion <- ifelse(abs(diag2$stats) > diag2$t_value, "reject H0", "don't reject H0")
   diag2$conclusion[3] <- ifelse(diag2$stats[3] > diag2$f_value[3], "reject H0", "don't reject H0")
   rownames(diag2) <- c("H0: alpha = 0", "H0: beta = 1", "H0: alpha = 0 e beta = 1")
   
@@ -130,11 +131,14 @@ core.diag <- function(y, core, par, conf = 0.95){
   out_coint <- ifelse(coint@teststat[2] > coint@cval[2,2], "coint", "not coint")
   
   # diag 5: atratividade ----
+  #data <- window(data, start = c(2007,8), freq = 12)
   reg <- lm(data[,1] ~ data[,2])
   erro <- ts(resid(reg), end = end(data), freq = 12)
   
-  difs <- na.omit(cbind(diff(y), diff(core), lag(erro, k = -1)))
-  colnames(difs) <- c("y","core","erro_1")
+  dum <- BETS::BETS.dummy(start = start(data), end = end(data), month = 12, year = 2002)
+  
+  difs <- na.omit(cbind(diff(y), diff(core), lag(erro, k = -1), dum))
+  colnames(difs) <- c("y","core","erro_1", "dummy")
   
   modelo_y <- dynlm(difs[,"y"] ~ difs[,"erro_1"] + L(difs[,"y"],1) +
                          L(difs[,"y"],2) + 
@@ -146,28 +150,49 @@ core.diag <- function(y, core, par, conf = 0.95){
                          #L(difs[,"y"],8) + 
                          #L(difs[,"y"],9) + 
                          #L(difs[,"y"],10) +
-                         #L(difs[,"y"],11) + 
+                         L(difs[,"y"],11) + 
                          L(difs[,"y"],12))
   summary(modelo_y)
   plot(modelo_y$residuals)
   acf(modelo_y$residuals, lag.max = 36)
   
-  modelo_core <- dynlm(difs[,"core"] ~ difs[,"erro_1"] + L(difs[,"core"],1) +
-                         #L(difs[,"core"],2) + 
+  modelo_core <- dynlm(difs[,"core"] ~ difs[,"erro_1"] + difs[,"dummy"] +
+                         L(difs[,"core"],1) +
+                         L(difs[,"core"],2) + 
                          L(difs[,"core"],3) +
-                         #L(difs[,"core"],4) + 
-                         L(difs[,"core"],5) +
-                         #L(difs[,"core"],6) +
-                         #L(difs[,"core"],7) +
-                         #L(difs[,"core"],8) + 
-                         #L(difs[,"core"],9) + 
-                         #L(difs[,"core"],10) +
-                         L(difs[,"core"],11) + 
-                         L(difs[,"core"],12))
+                          #L(difs[,"core"],4) + 
+                          #L(difs[,"core"],5) +
+                          L(difs[,"core"],6) +
+                          #L(difs[,"core"],7) +
+                          L(difs[,"core"],8) + 
+                          L(difs[,"core"],9) + 
+                          #L(difs[,"core"],10) +
+                          L(difs[,"core"],11) + 
+                          L(difs[,"core"],13))
   summary(modelo_core)
   plot(modelo_core$residuals)
   acf(modelo_core$residuals, lag.max = 36)
   # lambda_h < 0 e lambda_c > 0 -> as séries se ajustam uma a outra       
+  
+  # previsão
+  
+  pi12 <- lag(y, k = -12)
+  pic12 <- lag(core, k = -12)
+  d <- na.omit(cbind(y - pi12, pic12 - pi12))
+  d <- window(na.omit(cbind(y - pi12, pic12 - pi12)), start = c(2006,4), freq = 12)
+  colnames(d) <- c("y","x")
+  
+  md <- lm(d[,1] ~ d[,2])
+  summary(md)
+  
+  pi24 <- lag(y, k = -24)
+  pic24 <- lag(core, k = -24)
+  d <- na.omit(cbind(y - pi24, pic24 - pi24))
+  d <- window(na.omit(cbind(y - pi24, pic24 - pi24)), start = c(2006,4), freq = 12)
+  colnames(d) <- c("y","x")
+  
+  md <- lm(d[,1] ~ d[,2])
+  summary(md)
   
   # output
   list(stats = tabela_desc, bias.test = diag2, 
